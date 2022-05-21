77976c2512085fcfe1ff38a137394dbd502eda8f
Andrei Trasca <trascaandrei17@gmail.com>
2022-05-21 15:14:25 +0300

    init front-end app

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+## Quick start
+
+- Make sure your NodeJS and npm versions are up to date for `React 17`
+
+- Install dependencies: `npm install` or `yarn`
+
+- Start the server: `npm run dev` or `yarn dev`
+
+- Views are on: `localhost:3000`
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,64 @@
+import Head from 'next/head';
+import { Box, Container, Grid } from '@mui/material';
+import { DocumentsListResults } from '../components/dashboard/documents-list-results';
+import { DocumentsListToolbar } from '../components/dashboard/documents-list-toolbar';
+import { TasksProgress } from '../components/dashboard/tasks-progress';
+import { TotalDocuments } from '../components/dashboard/total-documents';
+import { DashboardLayout } from '../components/dashboard-layout';
+import { documents } from '../__mocks__/documents';
+
+
+const Dashboard = () => (
+  <>
+    <Head>
+      <title>
+        Dashboard | Material Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            xl={6}
+            lg={6}
+            sm={6}
+            xs={12}
+          >
+            <TotalDocuments />
+          </Grid>
+          <Grid
+            item
+            xl={6}
+            lg={6}
+            sm={6}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+        </Grid>
+        <DocumentsListToolbar sx={{ mt: 3 }} />
+        <Box sx={{ mt: 3 }}>
+          <DocumentsListResults documents={documents} />
+        </Box>
+      </Container>
+    </Box>
+  </>
+);
+
+Dashboard.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Dashboard;
