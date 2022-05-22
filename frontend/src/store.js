efb3fad313ed4a58fbe311f0ce7d49db6a734655
Andrei Trasca <trascaandrei17@gmail.com>
2022-05-22 21:06:29 +0300

    add and init redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,12 @@
+import { configureStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+const middleware = [thunk];
+
+const store = configureStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
