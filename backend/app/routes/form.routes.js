2a6c18619641c07cab4448d5f095fa0b5646f0d8
Radu Pirosca <radupirosca@gmail.com>
2022-05-29 11:34:50 +0300

    Generate form

---
+++ backend/app/routes/form.routes.js
@@ -0,0 +1,12 @@
+const controller = require("../controllers/form.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  app.get('/api/form/generate/:formId', controller.generateForm);
+};
